941e88236c91811428cfc98713f8cfd4a8a8fc27
CalebHolt-15 <calebholt100@gmail.com>
2020-10-20 19:05:16 +0530

    made minor changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import React, { PureComponent } from "react";
+import React, { memo } from "react";
+import PropTypes from "prop-types";
+import moduleName from "module";
 
-ReactDOM.render(
-    <>
-        hello world
-    </>,
-    document.getElementById('root')
-);
+ReactDOM.render(<>hello world</>, document.getElementById("root"));
