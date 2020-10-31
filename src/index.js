8ab7288fa54994dc938c73d6b604b0932a41a9d9
CalebHolt-15 <calebholt100@gmail.com>
2020-10-31 12:37:59 +0530

    Material UI set up completed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+import { Button, CssBaseline } from "@material-ui/core";
 import React from "react";
 import ReactDOM from "react-dom";
-import React, { PureComponent } from "react";
-import React, { memo } from "react";
-import PropTypes from "prop-types";
-import moduleName from "module";
 
-ReactDOM.render(<>hello world</>, document.getElementById("root"));
+ReactDOM.render(
+  <>
+    <CssBaseline />
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  </>,
+  document.getElementById("root")
+);
