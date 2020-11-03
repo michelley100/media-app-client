d954dc47062d33c88165b355f37d6be097060db6
CalebHolt-15 <calebholt100@gmail.com>
2020-11-03 15:17:54 +0530

    Get DATA from DB and display in the homescreen

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import { Button, Typography } from "@material-ui/core";
-import React from "react";
+import Axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Route } from "react-router-dom";
 
 export const Home = ({ history }) => {
@@ -7,16 +8,48 @@ export const Home = ({ history }) => {
     localStorage.removeItem("token");
     history.push("/signin");
   };
+
+  const [songs, setSongs] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getSongs = async () => {
+      const token = localStorage.getItem("token");
+      const options = {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        withCredentials: true,
+        url: "https://localhost:8090/song",
+      };
+      try {
+        const { data } = await Axios(options);
+        setSongs(data);
+        setLoading(false);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getSongs();
+  }, []);
+
   return (
     <>
       <Typography>Hello people</Typography>
+
+      {!loading &&
+        songs.map((song) => {
+          return <h1>{song.title}</h1>;
+        })}
+
       <Button
         fullWidth
         variant="text"
         color="secondary"
         onClick={() => history.push("/song")}
       >
-        Song
+        Add Songs
       </Button>
 
       <Button onClick={handleLogOut}>Log Out</Button>
